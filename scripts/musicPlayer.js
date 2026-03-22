fed48d891e50d80a0f560be7b15a4931b0e5ebcf
kamal-stark-dev <kamalveer356@gmail.com>
2026-03-22 14:12:25 +0530

    add version 1.0

---
+++ scripts/musicPlayer.js
@@ -0,0 +1,115 @@
+import { songs } from "../assets/songs/songs.js";
+
+const song = document.getElementById("song");
+
+const playPause = document.getElementById("playPause");
+const currentTimeEl = document.getElementById("currentTime");
+const durationEl = document.getElementById("duration");
+const songTitleEl = document.getElementById("song-title");
+
+const rewindEl = document.getElementById("rewind10");
+const forwardEl = document.getElementById("forward10");
+
+const prevSongEl = document.getElementById("prevSong");
+const nextSongEl = document.getElementById("nextSong");
+
+const trackImageEl = document.getElementById("track-image");
+
+// handling keydown events
+document.querySelector("body").addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    playPauseSong();
+  }
+  if (event.key === "ArrowRight") {
+    song.currentTime = Math.min(song.duration, song.currentTime + 10);
+  }
+  if (event.key === "ArrowLeft") {
+    song.currentTime = Math.max(0, song.currentTime - 10);
+  }
+  if (event.ctrlKey) {
+    if (event.key === "ArrowLeft") {
+      idx = (idx - 1 + songsLength) % songsLength;
+      changeSong(idx);
+    }
+    if (event.key === "ArrowRight") {
+      idx = (idx + 1) % songsLength;
+      changeSong(idx);
+    }
+  }
+});
+
+// song link
+let idx = 0;
+const songsLength = songs.length;
+
+function changeSong(idx = 0) {
+  trackImageEl.src = songs[idx].bannerLink;
+  song.getElementsByTagName("source")[0].src = songs[idx].songLink;
+  song.load(); // to load the new song
+  songTitleEl.innerText = songs[idx].songTitle;
+
+  playPause.classList.remove("ri-play-fill");
+  playPause.classList.add("ri-pause-fill");
+  song.play();
+}
+changeSong(idx);
+
+prevSongEl.addEventListener("click", () => {
+  idx = (idx - 1 + songsLength) % songsLength;
+  changeSong(idx);
+});
+
+nextSongEl.addEventListener("click", () => {
+  idx = (idx + 1) % songsLength;
+  changeSong(idx);
+});
+
+// song volume
+song.volume = 0.4;
+
+playPause.addEventListener("click", () => {
+  playPauseSong();
+});
+
+song.addEventListener("loadedmetadata", () => {
+  durationEl.innerText = formatTime(song.duration);
+});
+
+song.addEventListener("timeupdate", () => {
+  currentTimeEl.innerText = formatTime(song.currentTime);
+});
+
+// rewind and forward - 10 seconds
+rewindEl.addEventListener("click", () => {
+  song.currentTime = Math.max(0, song.currentTime - 10);
+});
+
+forwardEl.addEventListener("click", () => {
+  song.currentTime = Math.min(song.duration, song.currentTime + 10);
+});
+
+function playPauseSong() {
+  if (song.paused) {
+    playPause.classList.remove("ri-play-fill");
+    playPause.classList.add("ri-pause-fill");
+    song.play();
+  } else {
+    playPause.classList.remove("ri-pause-fill");
+    playPause.classList.add("ri-play-fill");
+    song.pause();
+  }
+}
+
+function formatTime(totalSeconds) {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = Math.floor(totalSeconds % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+// function getSongTitle() {
+//   const songLink = song.getElementsByTagName("source")[0].src;
+//   const songName = songLink
+//     .substring(songLink.lastIndexOf("/") + 1, songLink.lastIndexOf("."))
+//     .replace(/%20/g, " ");
+//   return songName;
+// }
---
+++ scripts/timer.js
@@ -0,0 +1,47 @@
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+
+const timerEl = document.getElementById("timer");
+
+let interval;
+const desiredTime = 10; // 25 minutes
+let timeLeft = desiredTime;
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+
+  const formattedTime =
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
+
+  timerEl.innerText = formattedTime;
+}
+
+function startTimer() {
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      timeLeft = desiredTime;
+      updateTimer();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  timeLeft = desiredTime;
+  updateTimer();
+}
+
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
